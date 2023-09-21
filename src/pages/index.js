3f796d105057fedaab001776b168d2e32b0c8a31
wildanaryo <wildanaryo@gmail.com>
2023-09-22 02:09:54 +0700

    edit feature search

--- src/pages/index.js
+++ src/pages/index.js
@@ -99,9 +99,11 @@ export default function Home() {
 
   const handleJumpLink = (num) => {
     fetchData(num);
+    setDataSearch("");
   };
 
   const handleFilterGender = (gender) => {
+    setDataSearch("");
     filterGender(gender);
     setSortConfig({ key: null, direction: "ascending" });
   };
@@ -144,6 +146,7 @@ export default function Home() {
           <div className="flex space-x-1">
             <input
               type="text"
+              value={dataSearch}
               className="mt-1 px-3 py-2 text-black bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
               placeholder="Search..."
               onChange={(e) => setDataSearch(e.target.value)}
